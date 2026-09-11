d20cb7aeabd4843e3fb864e861a2ea32d76de46b
Md. Arif Hassan <aarifhsn@gmail.com>
2026-09-11 23:48:59 +0600

    fixed few styling

--- template-parts/home/weekly-deals.php
+++ template-parts/home/weekly-deals.php
@@ -23,48 +23,58 @@
  * @package TechMart
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit;
 }
 
-$products = techmart_get_weekly_deal_products( 5 );
+$products = techmart_get_weekly_deal_products(5);
 
-if ( empty( $products ) ) {
+if (empty($products)) {
 	return;
 }
 
-$soonest_expiry = (int) get_post_meta( $products[0]->get_id(), '_sale_price_dates_to', true );
-$has_countdown  = $soonest_expiry > time();
+$soonest_expiry = (int) get_post_meta($products[0]->get_id(), '_sale_price_dates_to', true);
+$has_countdown = $soonest_expiry > time();
 ?>
-<section class="tm-band tm-weekly-deals" aria-label="<?php esc_attr_e( 'Weekly deals', 'techmart' ); ?>">
+<section class="tm-band tm-weekly-deals" aria-label="<?php esc_attr_e('Weekly deals', 'techmart'); ?>">
 	<div class="tm-container">
 		<div class="tm-section-header tm-weekly-deals__header">
-			<h2 class="tm-section-header__title"><?php esc_html_e( 'Weekly Deals', 'techmart' ); ?></h2>
+			<h2 class="tm-section-header__title"><?php esc_html_e('Weekly Deals', 'techmart'); ?></h2>
 
-			<?php if ( $has_countdown ) : ?>
-				<div class="tm-countdown" data-expires="<?php echo esc_attr( $soonest_expiry ); ?>">
-					<span class="tm-countdown__label"><?php esc_html_e( 'Ends in', 'techmart' ); ?></span>
-					<span class="tm-countdown__unit"><span data-unit="days">00</span><small><?php esc_html_e( 'd', 'techmart' ); ?></small></span>
-					<span class="tm-countdown__unit"><span data-unit="hours">00</span><small><?php esc_html_e( 'h', 'techmart' ); ?></small></span>
-					<span class="tm-countdown__unit"><span data-unit="minutes">00</span><small><?php esc_html_e( 'm', 'techmart' ); ?></small></span>
-					<span class="tm-countdown__unit"><span data-unit="seconds">00</span><small><?php esc_html_e( 's', 'techmart' ); ?></small></span>
+			<?php if ($has_countdown): ?>
+				<div class="tm-countdown" data-expires="<?php echo esc_attr($soonest_expiry); ?>">
+					<span class="tm-countdown__label">
+						<?php esc_html_e('Ends in', 'techmart'); ?>
+					</span>
+					<span class="tm-countdown__unit"><span data-unit="days">00</span><small>
+							<?php esc_html_e('d', 'techmart'); ?>
+						</small></span>
+					<span class="tm-countdown__unit"><span data-unit="hours">00</span><small>
+							<?php esc_html_e('h', 'techmart'); ?>
+						</small></span>
+					<span class="tm-countdown__unit"><span data-unit="minutes">00</span><small>
+							<?php esc_html_e('m', 'techmart'); ?>
+						</small></span>
+					<span class="tm-countdown__unit"><span data-unit="seconds">00</span><small>
+							<?php esc_html_e('s', 'techmart'); ?>
+						</small></span>
 				</div>
 			<?php endif; ?>
 
-			<a href="<?php echo esc_url( wc_get_page_permalink( 'shop' ) ); ?>" class="tm-section-header__link">
-				<?php esc_html_e( 'View All Deals', 'techmart' ); ?>
-				<?php techmart_icon( 'arrow-right' ); ?>
+			<a href="<?php echo esc_url(wc_get_page_permalink('shop')); ?>" class="tm-section-header__link">
+				<?php esc_html_e('View All Deals', 'techmart'); ?>
+				<?php techmart_icon('arrow-right'); ?>
 			</a>
 		</div>
 
 		<div class="tm-product-grid">
-			<?php foreach ( $products as $product ) : ?>
+			<?php foreach ($products as $product): ?>
 				<?php
 				techmart_get_template_part(
 					'template-parts/product/card',
 					null,
 					array(
-						'product'          => $product,
+						'product' => $product,
 						'show_stock_meter' => true,
 					)
 				);
